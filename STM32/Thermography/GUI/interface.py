720d3c5c59a07e1d84b5514fb5ed12b42b015c06
arao <araobp@users.noreply.github.com>
2023-09-06 17:22:24 +0900

    update

--- STM32/Thermography/GUI/interface.py
+++ STM32/Thermography/GUI/interface.py
@@ -3,20 +3,22 @@ import numpy as np
 import traceback
 import threading
 
-### Constants ###
+### Constants #####
 
-BAUD_RATE = 115200   # UART baud rate
+BAUD_RATE = 115200          # UART baud rate
 
 PIXELS = b'p'
-THERMISTER = b't'
+THERMISTOR = b't'
 
-NUM_SAMPLES = {}
+# main.c
+NUM_SAMPLES = {}            # The number of samples to receive from the device
 NUM_SAMPLES[PIXELS] = 64
-NUM_SAMPLES[THERMISTER] = 1
+NUM_SAMPLES[THERMISTOR] = 1
 
 class Interface:
-
+    
     def __init__(self, port):
+        # Serial interface
         self.port = port
         self.lock = threading.Lock()
         self.ser = None
@@ -25,11 +27,13 @@ class Interface:
         except:
             print('*** Serial connection failure!')
             traceback.print_exc()
-    
+
     def close(self):
         if self.ser:
             self.ser.close()
-    
+                
+    # As an application processor, send a command
+    # then receive and process the output.
     def read(self, cmd, ssub=None):
 
         data = []
@@ -37,15 +41,16 @@ class Interface:
             n = 0
             try:
                 self.ser.write(cmd)
-                rx = self.ser.read(NUM_SAMPLES[cmd] * 2)
+                rx = self.ser.read(NUM_SAMPLES[cmd]*2)
                 rx = zip(rx[0::2], rx[1::2])
                 for lsb, msb in rx:
                     n += 1
-                    d = int.from_bytes([msb, lsb], byteorder='big', signed=False)
+                    d =  int.from_bytes([msb, lsb], byteorder='big', signed=False)
                     data.append(d)
                 data = np.array(data, dtype=np.int16)
             except:
                 print('*** serial timeout!')
                 traceback.print_exc()
 
-        return data 
+        return data
+    

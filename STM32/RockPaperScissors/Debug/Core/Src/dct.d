026db4517f1b46a0796b3fd41a5e4b8deed5c006
arao <araobp@users.noreply.github.com>
2023-09-08 12:30:02 +0900

    update

--- STM32/RockPaperScissors/Debug/Core/Src/dct.d
+++
@@ -1,8 +0,0 @@
-Core/Src/dct.o: ../Core/Src/dct.c ../Core/Inc/dct.h \
- C:/Users/shiny/Documents/data-science-study/STM32/RockPaperScissors/Drivers/CMSIS/DSP/Include/arm_math.h \
- ../Drivers/CMSIS/Include/cmsis_compiler.h \
- ../Drivers/CMSIS/Include/cmsis_gcc.h
-../Core/Inc/dct.h:
-C:/Users/shiny/Documents/data-science-study/STM32/RockPaperScissors/Drivers/CMSIS/DSP/Include/arm_math.h:
-../Drivers/CMSIS/Include/cmsis_compiler.h:
-../Drivers/CMSIS/Include/cmsis_gcc.h:

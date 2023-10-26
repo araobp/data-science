9f952f280125fc8d12ae7a068c6867f94a7ce706
arao <araobp@users.noreply.github.com>
2023-10-27 00:02:56 +0900

    update

--- STM32/AcousticFeatureCamera/Core/Src/dsp.c
+++ STM32/AcousticFeatureCamera/Core/Src/dsp.c
@@ -133,15 +133,16 @@ void apply_pre_emphasis(float32_t *signal) {
 // AC coupling (to remove DC)
 void apply_ac_coupling(float32_t *signal) {
   static float32_t mean;
-  static int cnt = NUM_MEANS * 16;
+  static int cnt = NUM_MEANS * 2;
   static float32_t mean_hist[NUM_MEANS] = { 0.0f };
 
-  if (cnt >= 0) {
-    if (cnt-- <= NUM_MEANS * 8) {
+  if (cnt > 0) {
+    if (cnt-- <= NUM_MEANS) {
       arm_copy_f32(mean_hist + 1, mean_hist, NUM_MEANS - 1);
       arm_mean_f32(signal, NN, mean_hist + NUM_MEANS - 1);
-      arm_mean_f32(signal, NUM_MEANS, &mean);
     }
+  } else if (cnt-- == 0) {
+    arm_mean_f32(mean_hist, NUM_MEANS, &mean);
   } else {
     arm_offset_f32(signal, -mean, signal, NN);
   }
